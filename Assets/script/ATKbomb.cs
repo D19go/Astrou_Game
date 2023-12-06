03f13c1015a194d5f26a5dc2f0c2b8e65d4a6f33
D19go <diogo.s.costa19@gmail.com>
2023-12-06 00:17:03 -0300

    tem que configurar o bomber

--- Assets/script/ATKbomb.cs
+++ Assets/script/ATKbomb.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ATKbomb : MonoBehaviour
 {
+
+    [SerializeField] private float raycastDistance = 50f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,19 @@ public class ATKbomb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, raycastDistance))
+        {
+            // Verificar se o raio atingiu um objeto com a tag "Inimigo"
+            if (hit.collider.CompareTag("Player"))
+            {
+                
+            }
+            /*else
+            {
+                // Lógica para lidar com outras colisões detectadas pelo raycast
+                Debug.Log("Colisão detectada!");
+            }*/
+        }
     }
 }
